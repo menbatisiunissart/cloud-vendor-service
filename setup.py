5932824fa18898f01ed2550878135872ad5b7fc7
menbatisiunissart <aboulenger@gmail.com>
2023-02-17 20:15:55 +0000

    fix: updated install_requires = ['python-dotenv>=0.21.0', 'sagemaker>=2.132.0'],

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloud-vendor-service',
-    version='0.1.0',
+    version='0.1.1',
     description='Cloud vendor service (AWS, Google Cloud...)',
     author='',
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url='https://github.com/menbatisiunissart/cloud-vendor-service',
-    install_requires = ['python-dotenv==0.21.0'],
+    install_requires = ['python-dotenv>=0.21.0', 'sagemaker>=2.132.0'],
     packages=find_packages(),
 )
 
