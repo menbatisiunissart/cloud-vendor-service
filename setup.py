fbd8aee13923067f3590c824ba418e9f73db559b
menbatisiunissart <aboulenger@gmail.com>
2023-02-17 19:33:01 +0000

    fix: updated [packages] sagemaker = "*"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cloud-vendor-service',
-    version='0.0.11',
+    version='0.1.0',
     description='Cloud vendor service (AWS, Google Cloud...)',
     author='',
     author_email='',
